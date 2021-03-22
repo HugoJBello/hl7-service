cfee5700fabb26640ebd8fc58e6877953c956712
hugojbello <hugo@insulclock.com>
2021-03-22 19:21:32 +0100

    initial commit

--- src/controllers/hl7MessageController.ts
+++ src/controllers/hl7MessageController.ts
@@ -4,6 +4,8 @@ import { HL7MessageI } from "../models/HL7Message";
 
 export const hl7MessageController = async (req: Request, res: Response): Promise<void> => {
     const hl7Message = req.body as HL7MessageI
+    const payload = hl7Message.payload
+
 
     res.json({message:"Ok"});
 };
